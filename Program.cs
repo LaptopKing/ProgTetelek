e9ee79547f2ba393db1641a6006ec04a448f0410
LaptopKing <csordaspeter2002@gmail.com>
2021-02-01 13:56:55 +0100

    new file:   ProgTetelek.csproj 	new file:   Program.cs 	new file:   bin/Debug/net5.0/ProgTetelek 	new file:   bin/Debug/net5.0/ProgTetelek.deps.json 	new file:   bin/Debug/net5.0/ProgTetelek.dll 	new file:   bin/Debug/net5.0/ProgTetelek.pdb 	new file:   bin/Debug/net5.0/ProgTetelek.runtimeconfig.dev.json 	new file:   bin/Debug/net5.0/ProgTetelek.runtimeconfig.json 	new file:   bin/Debug/net5.0/ref/ProgTetelek.dll 	new file:   obj/Debug/net5.0/.NETCoreApp,Version=v5.0.AssemblyAttributes.cs 	new file:   obj/Debug/net5.0/ProgTetelek.AssemblyInfo.cs 	new file:   obj/Debug/net5.0/ProgTetelek.AssemblyInfoInputs.cache 	new file:   obj/Debug/net5.0/ProgTetelek.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net5.0/ProgTetelek.assets.cache 	new file:   obj/Debug/net5.0/ProgTetelek.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net5.0/ProgTetelek.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net5.0/ProgTetelek.csprojAssemblyReference.cache 	new file:   obj/Debug/net5.0/ProgTetelek.dll 	new file:   obj/Debug/net5.0/ProgTetelek.genruntimeconfig.cache 	new file:   obj/Debug/net5.0/ProgTetelek.pdb 	new file:   obj/Debug/net5.0/apphost 	new file:   obj/Debug/net5.0/ref/ProgTetelek.dll 	new file:   obj/ProgTetelek.csproj.nuget.dgspec.json 	new file:   obj/ProgTetelek.csproj.nuget.g.props 	new file:   obj/ProgTetelek.csproj.nuget.g.targets 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgTetelek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = new int[10] { 10, 25, 40, 123, 523, 1236, 51, 3100, 86, 91};
+
+            Max(test);
+            Kivalaszt(test);
+        }
+
+        public static void Max(int[] test)
+        {
+            int max = test[0];
+            for (int i = 0; i < test.Length; i++)
+            {
+                if (max < test[i])
+                {
+                    max = test[i];
+                }
+            }
+            Console.WriteLine(max + "\n");
+        }
+
+        public static void Kivalaszt(int[] test)
+        {
+            Console.Write("Adjon meg egy számot: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Adjon meg egy másik számot: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                int num3;
+                num3 = num1;
+                num1 = num2;
+                num2 = num3;
+            }
+
+            List<int> nums = new List<int>();
+
+            foreach (int num in test)
+            {
+                if ((num1 < num) && (num2 > num))
+                {
+                    nums.Add(num);
+                }
+            }
+            nums.Sort();
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
